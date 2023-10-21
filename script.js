acb91200ab2c9719cdec3ee63fcbf26ab509122c
DanioleQ <danoiwy1@gmail.com>
2023-10-21 19:45:34 +0200

    Added to script.js money display update function

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-let userMoney = 0;
+let userMoney = 200;
 let contentDiv = document.getElementById("myDiv");
 let button = document.getElementById("myButton");
 let upgradeButton = document.getElementById("upgradeButton");
 let upgradeLevel = 1;
 let upgradeDiv = document.getElementById("upgrade");
+// Money Update Function
+function updateMoneyDisplay() {
+  contentDiv.textContent = userMoney;
+}
+// Money Update by 1sec
+setInterval(updateMoneyDisplay);
 // Upgrade Function
 upgradeButton.addEventListener("click", function () {
   if (userMoney >= 200 && upgradeLevel < 10) {
     upgradeLevel++;
     userMoney = userMoney - 200;
     upgradeDiv.textContent = "Upgraded";
+    updateMoneyDisplay();
   } else {
     alert("Not enough money");
   }
@@ -17,5 +24,5 @@ upgradeButton.addEventListener("click", function () {
 // Main money button maker
 button.addEventListener("click", function () {
   userMoney = userMoney + upgradeLevel;
-  contentDiv.textContent = userMoney;
+  updateMoneyDisplay();
 });
